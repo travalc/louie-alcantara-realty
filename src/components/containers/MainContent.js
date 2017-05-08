64be1b1a19e1ae3d286bffeecbd26574855a6e91
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-08 10:59:37 -0700

    Added third image

--- src/components/containers/MainContent.js
+++ src/components/containers/MainContent.js
@@ -6,7 +6,8 @@ class MainContent extends Component {
     this.state = {
       currentImage: "FirstImage",
       classOne: 'img-loaded',
-      classTwo: 'image'
+      classTwo: 'image',
+      classThree: 'image'
     }
     this.imageChange = this.imageChange.bind(this);
   }
@@ -16,20 +17,30 @@ class MainContent extends Component {
       this.setState({
         currentImage: "SecondImage",
         classOne: 'image',
-        classTwo: 'img-loaded'
+        classTwo: 'img-loaded',
+        classThree: 'image'
       });
     }
-    else {
+    else if (this.state.currentImage === "SecondImage"){
+      this.setState({
+        currentImage: "ThirdImage",
+        classOne: 'image',
+        classTwo: 'image',
+        classThree: 'img-loaded'
+      });
+    }
+    else if (this.state.currentImage === "ThirdImage") {
       this.setState({
         currentImage: "FirstImage",
         classOne: 'img-loaded',
-        classTwo: 'image'
+        classTwo: 'image',
+        classThree: 'image'
       });
     }
   }
 
   componentDidMount() {
-    this.interval = setInterval(this.imageChange, 5000);
+    this.interval = setInterval(this.imageChange, 6000);
   }
   componentWillUnmount() {
     clearInterval(this.interval);
@@ -40,7 +51,8 @@ class MainContent extends Component {
     return (
       <div className="MainContent" style={{width: '100%', height: '600px'}}>
         <img className={this.state.classOne} id="FirstImage" src='http://www.thewallofchampions.com/wp-content/uploads/2013/04/2101.jpg' />
-        <img className={this.state.classTwo} id="SecondImage" src='http://wall--art.com/wp-content/uploads/2014/09/las-vegas-skyline-high-resolution.jpg' />
+        <img className={this.state.classTwo} id="SecondImage" src="http://www.davidmarquardtphotography.com/dm-old/wp-content/uploads/2011/11/Residential-Exterior-1.jpg" />
+        <img className={this.state.classThree} id="ThirdImage" src='http://www.lynamlaw.com/wp-content/uploads/2014/12/handshake.jpg' />
       </div>
     )
   }
