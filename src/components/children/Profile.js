46a267d17644e9ac78d94b9beb8190759c4170dc
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-08 12:46:06 -0700

    Finished profile component

--- src/components/children/Profile.js
+++ src/components/children/Profile.js
@@ -8,8 +8,19 @@ class Profile extends Component {
         <div className="about-louie">
           <p>
             A long time resident of Las Vegas and with over 15 years of experience in the local real estate market, Louie has
-            dedicated himself to providing an excellent experience to clients wishing to purchase or sell a home.
+            dedicated himself to providing an excellent experience to clients wishing to purchase, sell or keep a home. A principled man
+            and realtor, you can count on Louie to do what is best for you and your family.
           </p>
+        </div>
+        <div className="career-highlights">
+          <h3>Highlights</h3>
+          <ul>
+            <li>Foreclosure Alternative Consultant Specialist</li>
+            <li>Specialist in short sales since 2007 (95% success rate)</li>
+            <li>Excellent working relationships with major banks (Bank of America, Wells Fargo, Chase, Aurora, GMAC, etc.)</li>
+            <li>Experience in successfully helping clients to get short sales and loan modifications approved in court mediation proceedings</li>
+            <li>Track record in assisting distressed homeowners in retaining home ownership and avoid foreclosure.</li>
+          </ul>
         </div>
       </div>
     )
