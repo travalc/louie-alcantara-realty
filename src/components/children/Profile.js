51fb941fe530dc97e6158844b7f0f89824ca6394
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-08 12:02:05 -0700

    Began about section

--- src/components/children/Profile.js
+++ src/components/children/Profile.js
@@ -5,7 +5,12 @@ class Profile extends Component {
     return (
       <div className="Profile">
         <img className="img-louie" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/000/162/154/38f6dc2.jpg" />
-        
+        <div className="about-louie">
+          <p>
+            A long time resident of Las Vegas and with over 15 years of experience in the local real estate market, Louie has
+            dedicated himself to providing an excellent experience to clients wishing to purchase or sell a home.
+          </p>
+        </div>
       </div>
     )
   }
