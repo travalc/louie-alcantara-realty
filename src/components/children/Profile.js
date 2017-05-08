1dba336176f2ea0a93208dfe94d3d7083662a203
Travis Alcantara <travis.alcantara@gmail.com>
2017-05-08 11:14:50 -0700

    Added Profile component

---
+++ src/components/children/Profile.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Profile extends Component {
+  render() {
+    return (
+      <div className="Profile">
+        <img className="img-louie" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/000/162/154/38f6dc2.jpg" />
+      </div>
+    )
+  }
+}
+
+export default Profile;
--- src/components/containers/MainContent.js
+++ src/components/containers/MainContent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Profile from '../children/Profile'
 
 class MainContent extends Component {
   render() {
     return (
-      <div style={{backgroundColor:'white', width: '100%', height: '800px'}}>
-        <img className="img-louie" src="https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/4/000/162/154/38f6dc2.jpg" />
+      <div className="MainContent" style={{backgroundColor:'white', width: '100%', height: '800px'}}>
+        <Profile />
       </div>
     )
   }
